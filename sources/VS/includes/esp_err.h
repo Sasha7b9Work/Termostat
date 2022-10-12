881a48c72eee3c21b62cd29a5ea032ee0b7a0344
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-12 20:11:59 +0300

    Компилируется hello world в VS

---
+++ sources/VS/includes/esp_err.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define __attribute__
+#define noreturn
+
+typedef int32_t esp_err_t; 
---
+++ sources/VS/includes/esp_system.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+typedef enum {
+    CHIP_ESP8266 = 0, //!< ESP8266
+    CHIP_ESP32 = 1, //!< ESP32
+} esp_chip_model_t;
+
+#define CHIP_FEATURE_EMB_FLASH      BIT(0)      //!< Chip has embedded flash memory
+#define CHIP_FEATURE_WIFI_BGN       BIT(1)      //!< Chip has 2.4GHz WiFi
+#define CHIP_FEATURE_BLE            BIT(4)      //!< Chip has Bluetooth LE
+#define CHIP_FEATURE_BT             BIT(5)      //!< Chip has Bluetooth Classic
+
+typedef struct {
+    esp_chip_model_t model;  //!< chip model, one of esp_chip_model_t
+    uint32_t features;       //!< bit mask of CHIP_FEATURE_x feature flags
+    uint8_t cores;           //!< number of CPU cores
+    uint8_t revision;        //!< chip revision number
+} esp_chip_info_t;
+
+
+void esp_chip_info(esp_chip_info_t* out_info);
+void esp_restart(void);
