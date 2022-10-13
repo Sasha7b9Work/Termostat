77c517b4def86dedc263880a2608e50cacc74d78
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-13 22:40:19 +0300

    *

--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -4,10 +4,13 @@
 #include "Heater.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <driver/gpio.h>
 
 
 static void TaskLoop(void *cookie)
 {
+    static int counter = 0;
+
     while (1)
     {
         float temperature = 0.0f;
@@ -18,9 +21,11 @@ static void TaskLoop(void *cookie)
         {
             Heater_Process(temperature);
         }
-    }
 
-    vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(GPIO_NUM_15, counter++ % 2);
+
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
 }
 
 
@@ -31,6 +36,16 @@ extern "C" {
 
 void app_main()
 {
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_NUM_15;
+    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+
+    gpio_config(&io_conf);
+
+
     xTaskCreate(TaskLoop, "TaskLoop", 1024, NULL, 5, NULL);
 }
 
