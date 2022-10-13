b6715c00d638e33b12e03782560ceaf20cd80f40
Sasha7b9Work <Phuntik-it@tut.by>
2022-10-13 17:21:49 +0300

    *

--- sources/main/main.c
+++ sources/main/main.c
@@ -1,11 +1,4 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+// 2022/10/13 16:23:33 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
