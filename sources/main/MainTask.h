caf3732ea6ac5c3e33a6fabb03c3f6413fe3b26c
Sasha7b9Work <Phuntik-it@tut.by>
2022-10-14 13:31:08 +0300

    *

---
+++ sources/main/MainTask.h
@@ -0,0 +1,12 @@
+// 2022/10/14 13:11:46 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+namespace NS1
+{
+    class MainTask
+    {
+    public:
+        void Update();
+    };
+}
--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -1,4 +1,5 @@
 // 2022/10/13 16:23:33 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "MainTask.h"
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -11,20 +12,19 @@
 
 static void MainTask(void *cookie)
 {
+    NS1::MainTask task;
+
     while (1)
     {
-        static int counter = 0;
-
-        gpio_set_level(GPIO_NUM_2, counter++ % 2);
-
-        char *message = "Test message";
-
-        uart_write_bytes(UART_NUM_0, message, strlen(message) + 1);
+        task.Update();
 
         vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void app_main()
 {
@@ -37,17 +37,19 @@ void app_main()
 
     gpio_config(&io_conf);
 
-    uart_config_t uart_conf =
-    {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
+    uart_config_t uart_conf;
+    uart_conf.baud_rate = 115200;
+    uart_conf.data_bits = UART_DATA_8_BITS;
+    uart_conf.parity = UART_PARITY_DISABLE;
+    uart_conf.stop_bits = UART_STOP_BITS_1;
+    uart_conf.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
 
     uart_param_config(UART_NUM_0, &uart_conf);
     uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
 
     xTaskCreate(MainTask, "MainTask", 1024, NULL, 5, NULL);
 }
+
+#ifdef __cplusplus
+}
+#endif
